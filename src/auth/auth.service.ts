76e0790369da1686e410313df00e5be76ed22490
Lucas Rodrigues de Souza <Rodriguesdesouzal28@gmail.com>
2024-10-04 18:56:31 -0300

    feat: sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { UserService } from './../user/user.service';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  @Inject()
+  private readonly userService: UserService;
+
+  @Inject()
+  private readonly jwtService: JwtService;
+
+  async signIn(
+    params: Prisma.UserCreateInput,
+  ): Promise<{ access_Token: string }> {
+    const user = await this.userService.user({ email: params.email });
+    if (!user) throw new NotFoundException('User not found');
+
+    const passwordMath = await bcrypt.compare(params.password, user.password);
+    if (!passwordMath) throw new UnauthorizedException('Invalid credentials');
+
+    const payload = { sub: user.id };
+
+    return {
+      access_Token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
